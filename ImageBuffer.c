9a1387579fd0faf876365709d45cf06f00bbc116
jl2569 <jl2569@nau.edu>
2016-09-14 16:50:24 -0700

    started conversion process

--- ImageBuffer.c
+++ ImageBuffer.c
@@ -5,56 +5,101 @@
 	
 //}
 
-//void ppm3(input ,output){
-	
-//}
+void ppm3(FILE *input ,FILE *output){
+	char c = fgetc(input);
+	int counter  = 1;
+	char *com;
+	com = (char *)malloc(100000000);
+	com[0] = c;
+	while(1){
+		if (feof(input)){
+			break;
+		}
+		else{	
+			int c = fgetc(input);		
+			com[counter] = c;
+			
+			if(c == '\n'){
+				
+				counter +=1;
+			}
+			else{
+				counter+=1;
+			}
+			
+		}
+	}
+	printf("%s", com);
+}
+
+void same(FILE *input ,FILE *output){
+	char c = fgetc(input);
+	int counter  = 1;
+	char *com;
+	com = (char *)malloc(100000000);
+	com[0] = c;
+	while(1){
+		if (feof(input)){
+			break;
+		}
+		else{	
+			int c = fgetc(input);		
+			com[counter] = c;
+			
+			if(c == '\n'){
+				counter +=1;
+			}
+			else{
+				counter+=1;
+			}
+			
+		}
+	}
+	fputs(com,output);
+}
 
 int main(int argc, char *argv[]){
 	FILE *fh;
+	FILE *fp;
 	fh = fopen("title.ppm.ppm","r");
-	//fp = fopen("file.ppm","w");
-	int c = fgetc(fh);
+	fp = fopen("file.ppm","w");
+	char c = fgetc(fh);
 	int counter  = 1;
 	int tracker= 0;
-	char str[45];
+	char *str;
+	str = (char *)malloc(100);
 	str[0] = c;
 	
 	if(c != 'P' ){
 		fprintf(stderr ,"Error: This is not a ppm file! \n");
 	}
-	
 	while(1){
-		if (tracker == 3){
+		if (tracker == 4){
 			break;
 		}
-		else if(c == "#"){
-			while(c!= "\n"){
-			  int c = fgetc(fh);
+		else{
+			int c = fgetc(fh);
+			str[counter] = c;
+			if(c == '\n'){
+				tracker+=1;
+				counter+=1;
+			}
+			else{
+				counter+=1;
 			}
 		}
-		else{
-		int c = fgetc(fh);
-		str[counter] = c;
-		if(counter == "\n"){
-			tracker+=1;
-			counter+=1;
-		}
-		else{
-			counter+=1;
-		}
-		}
 	}
-	printf("%s",str);	
-	//printf("%c", c);
-	//while(1){
-	//	int c = fgetc(fh);
-	//	if(counter == 48){
-	//		break;
-	//	}
-	//	printf("%c", c);
-	//	counter +=1;
-	//}
-	
+	//printf("%c",str[1]);
+	//fputs(str,fp);
+	if (str[1] == '3'){
+		ppm3(fh,fp);
+	}
+//	else if(str[1]== '6'){
+//		ppm6(fh,fp,c);
+//	}
+	else{
+		fprintf(stderr ,"Error: Does not support images not P6 or P3 \n");
+	}
 	unsigned char gray = 30;
 	fprintf(fh,"pbh");
 	fprintf(fh, "%d ", gray);
